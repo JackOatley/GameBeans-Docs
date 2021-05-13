7b77e02ef1480f3b77201e8aed53cd2ef9e3d096
JackOatley <jack.f.oatley@gmail.com>
2021-05-13 10:12:06 +0100

    Restyled code reference pages

--- funcs.js
+++
@@ -1,8 +0,0 @@
-function showExample(e) {
-    var x = e.nextElementSibling.style;
-    if (x.display === "block") {
-        x.display = "none";
-    } else {
-        x.display = "block";
-    }
-}
